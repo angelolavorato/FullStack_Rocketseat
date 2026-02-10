994f333a678ed9bc5af2e0fffbb5714a392a8af5
angelolavorato <angelolavorato@hotmail.com>
2026-02-09 23:24:24 -0300

    ALO

--- JavaScript/Variaveis/scripts.js
+++ JavaScript/Variaveis/scripts.js
@@ -4,3 +4,8 @@ console.log(user) //undefined
 
 //Declarando uma variável com valor
 var email = 'angelo@email.com'
+console.log(email) //
+
+//substituindo o valor da variável
+email = 'pedro@email.com.br'
+console.log(email)
